2e71c10e8731e5a36a89adb3678695ce0e3d64d1
meow <woem>
2021-10-13 15:32:37 +0300

    Complete rewrite

--- README.md
+++ README.md
@@ -1,36 +1,20 @@
 # signimg2img
-![GitHub](https://img.shields.io/github/license/R0rt1z2/signimg2img)
-![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/R0rt1z2/signimg2img?include_prereleases)
 
-Convert signed Android images (.img-sign) into standard Android Images (.img)
+Convert signed Android images (-sign.img) into standard Android Images (.img)
+
+## Fork reasons
+
+[Original](https://github.com/R0rt1z2/signimg2img) package once eat all of my memory while copying image.
+So I investigated sources and find a bunch of absolutely bad stuff (loading heavy files (2-4 GiB) into memory without need,
+dependency on Linux-specific `dd` utility) and absolutely unrelated to script's main purpose (silent file deletion,
+silent mounting).  
+Then I was killed by literal `Windows is unable to unpack images with SSSS header!`  
+
+So, I removed everything not related to header cutting. Welcome to the sources.  
+Also now you may use this tool with pipes. Details inside.
 
 ## Requirements
-This binary requires Python 3 or newer installed on your system. 
-It currently supports Windows, Linux and MacOS architectures.
-
-## Usage
-```
-signimg2img.py -u <signed_image>
-signimg2img.py -o <signed_image>
-signimg2img.py -c
-```
-- `<signed_image>` = input, image-sign.img
-
-## Example
-This is a simple example on a Linux system: 
-```
-r0rt1z2@r0rt1z2: /signimg2img$ python3 signimg2img.py -u recovery-sign.img
-signimg2img binary - version: 1.5
-
-[signimg2img-log] Selected image: recovery-sign.img
-[signimg2img-log] Image header: b'BFBF'
-[signimg2img-log] Image header string: 'recovery' (0x000010)
-[signimg2img-log] Removing old files...
-[signimg2img-log] Deleting the header...
-[signimg2img-log] Done, image extracted as recovery.img
-
-r0rt1z2@r0rt1z2: /signimg2img$
-```
+This script requires Python 3.6+ installed on your system.
 
 ## Supported headers
 * BFBF
@@ -41,3 +25,7 @@ r0rt1z2@r0rt1z2: /signimg2img$
 
 ## Other information
 * To sign modified images you can use MediaTek's propietary tools: https://github.com/R0rt1z2/vendor_mediatek_signtool
+* There is another utility for same purpose [FbWinTools](http://lenovo-forums.ru/topic/16469--/).
+It can pack unpacked images back.
+* After removing sing header, you may want to desparse image. Use [simg2img](https://github.com/anestisb/android-simg2img).
+My builds for Windows are located [there](https://github.com/AHOHNMYC/android-simg2img/releases).
--- signimg2img.py
+++ signimg2img.py
@@ -1,190 +1,56 @@
 #!/usr/bin/env python3
-
-         #====================================================#
-         #              FILE: signimg2img.py                  #
-         #              AUTHOR: R0rt1z2                       #
-         #              DATE: 2019-2021                       #
-         #====================================================#
-
-#   Android signed images extractor v1.5. To use the script:
-#   "python3 signimg2img.py -u image_name to unpack any other image.
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-#   Thanks to anestisb (simg2img), no copyright asserted by them.
-
-from subprocess import *
-import struct
+import pathlib
 import sys
-import glob
-import time
-import shutil
-import os
 
-__version__ = '1.5'
+if __name__ == '__main__':
+    print = lambda s: sys.stderr.write(s+'\n')
 
-if sys.platform.startswith("darwin") or sys.platform.startswith("win") or sys.platform.startswith("linux"):
-    pass
-else:
-    raise Exception("Unsupported platform")
+    if len(sys.argv) != 3:
+        print('                     signimg2img v0.1')
+        print('Script for removing FBFB and SSSS headers from Android image files')
+        print('          https://github.com/AHOHNMYC/signimg2img')
+        print('')
+        print('USAGE:   signimg2img.py <signed_image> <raw_image_file>')
+        print('EXAMPLE: signimg2img.py image-sign.img image.img')
+        print('             take image-sign.img, process, save as image.img')
+        print('         signimg2img.py - -')
+        print('             take data from stdin, send to stdout')
+        exit(1)
 
-if sys.version_info[0] != 3:
-    raise Exception("Python 3.x or newer is required")
+    if not sys.argv[1] == '-':
+        if not pathlib.Path(sys.argv[1]).exists():
+            print('Input file does not exists. Exiting')
+            exit(1)
 
-def display(s):
-    text = "[signimg2img-log] {}".format(s)
-    print(text)
+    if not sys.argv[2] == '-':
+        if pathlib.Path(sys.argv[2]).exists():
+            print('Output file already exists. Exiting')
+            exit(1)
 
-def shCommand(sh_command, output):
-    if output == 0:
-        call(sh_command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    in_file = sys.stdin.buffer if sys.argv[1] == '-' else open(sys.argv[1], 'rb')
+    out_file = sys.stdout.buffer if sys.argv[2] == '-' else open(sys.argv[2], 'wb')
+
+    header = in_file.read(4)
+
+    # Seek from file start (0), not current position (4)
+    if header == b'BFBF':
+        # After 0x4000 BFBF header goes 0x40 SSSS header. Cut 'em both!
+        # in_file.seek(0x4040)
+        in_file.read(0x4040-4)
+    elif header == b'SSSS':
+        # in_file.seek(0x40)
+        in_file.seek(0x40-4)
     else:
-        call(sh_command, shell=True)
+        print(f"Unknown image header: {header.decode(errors='ignore')} ({header})")
+        print('Known headers are: BFBF, SSSS')
+        print('Try to use more powerful tool: Android Image Kitchen https://forum.xda-developers.com/t/2073775/')
+        exit(1)
 
-def get_offset(image):
-    image = open(image, 'rb')
-    image.read(60)
-    offset = struct.unpack('<I', image.read(4))[0]
-    return offset
+    # Copy by 16 MiB chunk
+    while out_file.write(in_file.read(16 * 1024 * 1024)):
+        pass
 
-def delete_header(image, outimage, hdr_type, offset):
-    display("Deleting the header...")
-    if hdr_type == "BFBF":
-       with open(image, 'rb') as in_file:
-          with open(outimage, 'wb') as out_file:
-            out_file.write(in_file.read()[16448:])
-    elif hdr_type == "SSSS":
-       if sys.platform.startswith("win"):
-          raise RuntimeError("Windows is unable to unpack images with SSSS header!")
-       shCommand("dd if={} of={} iflag=count_bytes,skip_bytes bs=8192 skip=64 count={}".format(image, outimage, offset), 0)
-       display("Header remove complete!")
-    else:
-       raise Exception("Invalid header: {}!".format(hdr_type))
-
-def regen_folder(folder):
-    try:
-        os.rmdir(folder)
-    except OSError:
-        shutil.rmtree(folder)
-    os.mkdir(folder)
-
-def check_header(image):
-    try:
-        with open(image, "rb") as image:
-            image_header = image.read(4)
-            image.seek(0x000010) 
-            try:
-                img_string = (image.read(8)).decode("utf-8")
-            except UnicodeDecodeError:
-                display("Warning: Cannot parse name string..")
-        display("Image header: {}".format(image_header))
-        if image_header == b'BFBF':
-            display("Image header string: '{}' (0x000010)".format(img_string))
-            header = "BFBF"
-        elif image_header == b'SSSS':
-            header = "SSSS"
-        else:
-            display("This is not a signed image!!\n")
-            exit()
-        return header
-    except FileNotFoundError:
-        raise RuntimeError("Couldn't open {}!".format(image))
-        exit()    
-
-def unpack_system(header):
-    if header == "BFBF":
-        delete_header("system-sign.img", "system.img", header, 0)
-    elif header == "SSSS":
-        display("Getting the offset...")
-        offset = get_offset("system-sign.img")
-        display("Image offset: {}".format(offset))
-        delete_header("system-sign.img", "system.img", header, offset)
-    display("Converting to ext4 image...")
-    p = Popen("simg2img system.img system.ext4", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
-    if (len(p.stderr.read()) != 0):
-        raise RuntimeError("Something went wrong while converting to ext4 image. Is simg2img installed? Bailing out...")
-    display("Unpacking system image...")
-    if os.path.exists("system_out"):
-        shCommand("sudo umount system_out", 0)
-        regen_folder("system_out")
-    else:
-        os.mkdir("system_out")
-    shCommand("mount -r -t ext4 -o loop system.ext4 system_out", 0)
-    display("system-sign.img extracted at system_out\n")
-    exit()
-
-def remove_old_files(image):
-    display("Removing old files...")
-    if os.path.isfile("signimg2img.py"):
-        for file in glob.glob('*.*'):
-            if image == "full":
-                if file.startswith("signimg2img") or file.startswith("LICENSE") or file.startswith("README"):
-                    pass
-                else:
-                    os.remove(file)
-            elif file.endswith(".ext4"):
-                os.remove(file)
-            elif file.startswith("signimg2img") or file.endswith("-sign.img") or file.startswith("LICENSE") or file.startswith("README"):
-                pass
-            else:
-                os.remove(file)
-
-def help():
-    display("USAGE: signimg2img.py -option:\n")
-    print("   -u image-sign.img: Removes the signed header from the given image. (system images will be automatically unpacked)")
-    print("   -o: Get image info (-o image_name).")
-    print("   -c: Full cleanup (removes all!).")
-    print("")
-    exit()
-
-def main():
-    print('signimg2img binary - version: {}\n'.format(__version__))
-    if len(sys.argv) == 1:
-        display("Expected more arguments.\n")
-        help()
-    elif sys.argv[1] == "-h":
-        help()
-    elif sys.argv[1] == "-u" and len(sys.argv) != 1:
-        display("Selected image: {}".format(sys.argv[2]))
-        header = check_header(sys.argv[2])
-        #remove_old_files(sys.argv[2])
-        if "system" in sys.argv[2]:
-            unpack_system(header)
-        else:
-            if "-sign" in sys.argv[2]:
-                output_image = sys.argv[2].replace("-sign", "")
-            else:
-                output_image = sys.argv[2] + ".unpack"
-            delete_header(sys.argv[2], "{}".format(output_image), header, 0)
-            display("Done, image extracted as {}\n".format(output_image).replace("-sign", ""))
-    elif sys.argv[1] == "-o":
-        header = check_header(sys.argv[2])
-        if header == "SSSS":
-            offset = get_offset(sys.argv[2])
-            display("Offset: {}".format(offset))
-        display("Size: {} bytes\n".format(os.path.getsize(sys.argv[2])))
-    elif sys.argv[1] == "-c":
-        remove_old_files("full")
-        if os.path.exists("system_out"):
-            p = Popen("umount system_out", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
-            if(len(p.stderr.read()) != 0):
-                display("(W) Cannot unmount the system_out folder!")
-            shutil.rmtree("system_out")
-        display("Cleaned up!\n")    
-    else:
-        display("Invalid option: {}\n".format(sys.argv[1]))
-        help()
-
-if __name__ == "__main__":
-    main()
+    print('Headers removed successfully!')
+    print('Now you may desparse *system* image using simg2img tool: https://github.com/anestisb/android-simg2img')
+    if sys.platform.startswith('win'):
+        print('Unofficial Windows builds: https://github.com/AHOHNMYC/android-simg2img/releases')
